de2c7636e087cf7d43bb53852dcf3bbcd2372f7d
Innokentie <69416693+Innokentie@users.noreply.github.com>
2021-02-09 00:11:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,51 @@ async def hello(ctx):
 bot.remove_command('help')
 @bot.command()
 async def help(ctx):
-	embed = discord.Embed(color = 0x537cda, description = f"\n**Информация**\n`!hello` - Приветствие.\n\n`!help` - Список моих команд.\n\n`!bot_info` - Информация обо мне.\n\n**Полезное**\n`!installer <Язык или файл> <Версия>` - Отправит указанный файл или даст ссылку на инсталлятор указанного языка программирования.\nНа данный момент доступны инсталляторы языка Python версий **2.0.1, 3.4.0, 3.5.0, 3.6.0, 3.7.0, 3.8.0, 3.9.1**.\n\n**Модерация**\n`!warn <Ник участника | @Упоминание | ID> <Причина>` - Выдаст предупреждение участнику без пинга, ни на что не влияет.\nКоманда доступна только модераторам.\n\n**Прочие**\n`!print <Текст>` - Команда выведет текст.\n\n`!ping <Ник участника | @Упоминание | ID> <std>` - Пинг указанного участника, не злоупотреблять коммандой!\n\nПредлагайте свои команды для меня в <#782893317226233857>.", title = 'Команды')
+	embed = discord.Embed(color = 0x537cda, description = f"\n**Информация**\n`!help` - Список моих команд.\n`!bot_info` - Информация обо мне.\n\n**Полезное**\n`!installer <Язык или файл> <Версия>` - Отправит указанный\nфайл или даст ссылку на инсталлятор указанного языка программирования.\nСейчас доступны инсталляторы языка Python\nверсий 2.0.1, 3.4.0, 3.5.0, 3.6.0, 3.7.0, 3.8.0, 3.9.1.\n\n**Модерация**\n`!ban <Участник | @Участник | ID> <Причина>` - Забанит участника на сервере.\n`!kick <Участник | @Участник | ID> <Причина>` - Выгонит\nучастника с сервера.\n`!warn <Участник | @Участник | ID> <Причина>` - Отправит в канал сообщение с предупреждением участнику.\n\n**Прочие**\n`!hello` - Сообщение с приветствием :)\n`!print <Текст>` - Команда отправит указаный текст\nи удалит сообщение с командой.", title = 'Команды')
 	await ctx.send(embed = embed)
 
 # Bot info
 @bot.command()
 async def bot_info(ctx):
-	embed = discord.Embed(color = 0x537cda, description = f"Меня разрабатывает <@677453905227022349>. У меня нет имени, но Вы пожете предложить его написав моему создателю или в канале <#782893317226233857>.\nЯ выполняю некоторые полезные команды, например я могу дать прямую ссылку на инсталлятор некоторых языков программирования или отправить файл, подробнее - `!help`.\nВы также можете вынести предложение о моем улучшении всё в том же канале <#782893317226233857>.", title = 'Обо мне')
-	embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/774646187440865290/9201269a8a69a50df77376ccd376fc53.png?size=256")
+	embed = discord.Embed(color = 0x537cda, description = f"Мой создатель - <@677453905227022349>.\nВы можете предложить имя для меня написав моему создателю или в канале <#782893317226233857>.\n\nУ меня открытый исходный код, вот мой репозиторий на Git Hub:\n`https://github.com/Innokentie/ours-bot`.", title = 'Обо мне')
+	embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/774646187440865290/643380e6d2dd7bbbe0aaae4c2815382c.png?size=256")
 	await ctx.send(embed = embed)
 
 # MODERATION
 
+# Ban
+@bot.command()
+@commands.has_any_role(782860394297294858, 782860246321856534, 782860339342737408)
+async def ban(ctx, member: discord.Member, *, about: str):
+	if member:
+		await member.ban()
+		embed = discord.Embed(color = 0x537cda, description = f'Участник **{member.name}** был забанен **{ctx.message.author.name}** по причине:\n**```\n{about}\n```**')
+		await ctx.send(embed = embed)
+
+	else:
+		embed = discord.Embed(color = 0x537cda, description = 'Ошибка в аргументах команды\nили участник не найден.', title = 'Ошибка')
+		await ctx.send(embed = embed)
+
+
+# Kick
+@bot.command()
+@commands.has_any_role(782860394297294858, 782860246321856534, 782860339342737408)
+async def kick(ctx, member: discord.Member, *, about: str):
+	if member:
+		await member.kick()
+		embed = discord.Embed(color = 0x537cda, description = f'Участник **{member.name}** был кикнут **{ctx.message.author.name}** по причине:\n**```\n{about}\n```**')
+		await ctx.send(embed = embed)
+
+	else:
+		embed = discord.Embed(color = 0x537cda, description = 'Ошибка в аргументах команды\nили участник не найден.', title = 'Ошибка')
+		await ctx.send(embed = embed)
+
 # Warn
 @bot.command()
+@commands.has_any_role(782860394297294858, 782860246321856534, 782860339342737408)
 async def warn(ctx, member: discord.Member, *, about: str):
 		if member:
-			embed = discord.Embed(color = 0x537cda, description = f'Учасмрапаретник **{member.name}** получил предупреждение от **{ctx.message.author.name}** по причине:\n**```\n{about}\n```**')
+			embed = discord.Embed(color = 0x537cda, description = f'Участник **{member.name}** получил предупреждение от **{ctx.message.author.name}** по причине:\n**```\n{about}\n```**')
 			await ctx.send(embed = embed)
 		else:
 			embed = discord.Embed(color = 0x537cda, description = 'Ошибка в аргументах команды\nили участник не найден.', title = 'Ошибка')
@@ -83,33 +111,23 @@ async def installer(ctx, name, version):
 		embed = discord.Embed(color = 0x537cda, description = f'Ошибка в аргументах команды\nили в моей БД нет данных файлов.', title = 'Ошибка!')
 		await ctx.send(embed = embed)
 
+# Vote
+@bot.command()
+async def vote(ctx):
+	
+	embed = discord.Embed(color = 0x537cda, description = f'**{question}**\n\n{varone}\n{vartwo}\n{varthree}\n{varfour}', title = '')
+	await ctx.send(embed = embed)
+
 # ALSO
 
-# Print    Сделать чтобы выдавался текст в ()
+# Print
 @bot.command()
 async def print(ctx, *, text):
-    await ctx.send(f'{text}')
-
-# Ping member
-@bot.command()
-async def ping(ctx, member: discord.Member, loop: str):
-	if loop == "std":
-		await ctx.channel.purge(limit=1)
-		await ctx.send(f'{member.mention}')
-
-	elif loop == "whileTrue":
-		while True:
-			await ctx.send(f'{member.mention}')
-
-	else:
-			await ctx.send('Ошибка в аргументе цикла!')
+	await ctx.channel.purge(limit=1)
+	await ctx.send(f'{text}')
 
 # SECRET
 
-# Sc 0000001
-@bot.command()
-async def sc0000001(ctx):
-	embed = discord.Embed(color = 0x537cda, description = f"Секретная команда", title = 'Секретная команда')
-	await ctx.send(embed = embed)
+# Hehe, hacker
 
-bot.run('TOKEN')
+bot.run('token')
